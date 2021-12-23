44b2ff674d7a2908a717ae68ba2d32316e633ec8
Kate Huang <katejhuang@hotmail.com>
2021-12-23 15:17:10 -0800

    create NotFound404 component

---
+++ src/components/NotFound404.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound404 = () => {
+    return (
+        <div>
+            Sorry! This page doesn't seem to exist!
+        </div>
+    );
+}
+
+export default NotFound404;

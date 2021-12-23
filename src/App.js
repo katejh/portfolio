bbdf72a68692533731f00d2c9dc2c8813b5a6678
Kate Huang <katejhuang@hotmail.com>
2021-12-23 11:37:50 -0800

    implement fetching data from resume.json

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+// import components
 import HomePage from './components/HomePage';
 import { Header } from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Header />
-        <Switch>
-          <Route path="/projects" />
-          <Route path="/resume" />
-          <Route exact path="/" component={HomePage} />
-        </Switch>
-      </Router>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      resumeData: {}
+    }
+  }
+
+  setResumeData() {
+    fetch('/resume.json',
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        }
+      } 
+    )
+      .then(r => r.json())
+      .then(data => {
+        this.setState({resumeData: data});
+        //console.log(this.state.resumeData);
+        //console.log(data);
+      });
+  }
+
+  componentDidMount() {
+    this.setResumeData();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Header />
+          <Switch>
+            <Route path="/projects" />
+            <Route path="/resume" />
+            <Route exact path="/" render={() => <HomePage data={this.state.resumeData}></HomePage>} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
 class HomePage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            resumeData: props.data
+        }
+        console.log(this.props.data);
+        console.log(this.state.resumeData);
+    }
 
     render() {
+        var name = this.state.resumeData.basic_info.name;
         return (
-            <div className="home-page">this do be the home page
+            <div className="home-page">
+            Hello! I am {name}
             <a href="resume.pdf" download>Resume download</a>
             </div>
         );

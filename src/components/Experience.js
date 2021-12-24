ac4350602b49ab01fb5480efca4dc9f70ea25aef
Kate Huang <katejhuang@hotmail.com>
2021-12-23 17:48:53 -0800

    create ExperienceTimelineElement component

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
-import { Badge } from 'react-bootstrap';
+import { Badge, Collapse } from 'react-bootstrap';
+import ExperienceTimelineElement from './ExperienceTimelineElement';
 
 import 'react-vertical-timeline-component/style.min.css';
 
@@ -9,36 +10,15 @@ class Experience extends React.Component {
         super(props);
 
         this.state = {
-            experience: props.experience
+            experience: props.experience,
         }
     }
 
     render() {
         var jobs = this.state.experience.map((job, i) => {
-            var tech = job.technologies.map((skill, i) => {
-                return (
-                    <Badge pill key={i}>
-                        {skill.name}
-                    </Badge>
-                );
-            });
-            var timeline_date_string = job.start_date + " - " + (job.end_date ? job.end_date : "Current");
-
-            return (
-                <VerticalTimelineElement
-                  className = "vertical-timeline-element--work"
-                  date={timeline_date_string}
-                  key={i}
-                >
-                    <h3 className="vertical-timeline-element-title">
-                        {job.company}
-                    </h3>
-                    <h4 className="vertical-timeline-element-subtitle">
-                        {job.title}
-                    </h4>
-                    <p>{job.brief}</p>
-                </VerticalTimelineElement>
-            );
+           return (
+            <ExperienceTimelineElement job={job}/>
+           );
         });
 
         return (
---
+++ src/components/ExperienceTimelineElement.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { VerticalTimelineElement } from 'react-vertical-timeline-component';
+import { Badge, Collapse } from 'react-bootstrap';
+
+// stylesheets
+import 'react-vertical-timeline-component/style.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class ExperienceTimelineElement extends React.Component {
+    constructor(props) {
+        super(props);
+
+        const job = props.job;
+
+        this.state = {
+            company: job.company,
+            title: job.title,
+            start_date: job.start_date,
+            end_date: job.end_date,
+            technologies: job.technologies,
+            brief: job.brief,
+            description: job.description,
+            open: false
+        }
+    }
+
+    render() {
+        var tech = this.state.technologies.map((skill, i) => {
+            return (
+                <Badge pill className="m3 bg-secondary" key={i}>
+                    {skill.name}
+                </Badge>
+            );
+        });
+        var timeline_date_string = this.state.start_date + " - " + (this.state.end_date ? this.state.end_date : "Current");
+
+        var description_collapsible = this.state.description ?
+        <Collapse in={this.state.open}>
+            <p>{this.state.description}</p>
+        </Collapse> 
+        : null;
+
+        return (
+            <VerticalTimelineElement
+                  className = "vertical-timeline-element--work"
+                  date={timeline_date_string}
+                  onTimelineElementClick={() => {this.setState({open: !this.state.open})}}
+                >
+                    <h3 className="vertical-timeline-element-title">
+                        {this.state.company}
+                    </h3>
+                    <h4 className="vertical-timeline-element-subtitle">
+                        {this.state.title}
+                    </h4>
+                    <div>{tech}</div>
+                    <p>{this.state.brief}</p>
+                    {description_collapsible}
+                </VerticalTimelineElement>
+        );
+    }
+}
+
+export default ExperienceTimelineElement;

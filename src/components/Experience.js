24536a457e32323392b214b302e934b3ef19cf70
Kate Huang <katejhuang@hotmail.com>
2021-12-25 18:46:33 -0800

    restructure experience section of resume.json and add summary

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { VerticalTimeline } from 'react-vertical-timeline-component';
 import ExperienceTimelineElement from './ExperienceTimelineElement';
+import { Card, CardContent, Typography } from '@mui/material';
 
 import 'react-vertical-timeline-component/style.min.css';
 
@@ -9,19 +10,31 @@ class Experience extends React.Component {
         super(props);
 
         this.state = {
-            experience: props.experience,
+            jobs: props.experience.jobs,
+            summary: props.experience.summary
         }
     }
 
     render() {
-        var jobs = this.state.experience.map((job, i) => {
+        var jobs = this.state.jobs.map((job, i) => {
            return (
             <ExperienceTimelineElement job={job} key={i}/>
            );
         });
 
+        var summaryCard = this.state.summary ? 
+        <div style={{display: 'flex', margin: 20, marginTop: 50, marginLeft: 70}}>
+            <Card sx={{minWidth: 200}}>
+                <CardContent>
+                    <Typography variant="h6">Summary</Typography>
+                    <Typography variant="p">{this.state.summary}</Typography>
+                </CardContent>
+            </Card>
+        </div> : null;
+
         return (
             <div className="Experience">
+                {summaryCard}
                 <VerticalTimeline>
                     {jobs}
                 </VerticalTimeline>

1febda172c0f2d44988fd91924bbe8de4a2d508d
Kate Huang <katejhuang@hotmail.com>
2021-12-29 09:01:38 -0800

    add title to Experience page and some styling

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { VerticalTimeline } from 'react-vertical-timeline-component';
 import ExperienceTimelineElement from './ExperienceTimelineElement';
-import { Card, CardContent, Typography } from '@mui/material';
+import { Card, CardContent, Typography, Box } from '@mui/material';
 
 import 'react-vertical-timeline-component/style.min.css';
 
@@ -23,22 +23,43 @@ class Experience extends React.Component {
         });
 
         var summaryCard = this.state.summary ? 
-        <div style={{display: 'flex', margin: 20, marginTop: 50, marginLeft: 70}}>
-            <Card sx={{minWidth: 200}}>
+            <Card 
+              sx={{
+                minWidth: {
+                    xs: 200,
+                    sm: 200,
+                    md: 400,
+                    lg: 400,
+                    xl: 400
+                }
+              }}
+            >
                 <CardContent>
                     <Typography variant="h6">Summary</Typography>
                     <Typography variant="p">{this.state.summary}</Typography>
                 </CardContent>
-            </Card>
-        </div> : null;
+            </Card> : null;
 
         return (
-            <div className="Experience">
+            <Box sx={{m: 4}}>
+                <div style={{}}>
+                    <Typography variant="h4">
+                        Experience
+                    </Typography>
+                </div>
+                <Box 
+                  sx={{
+                    display: 'flex',
+                    marginTop: 4,
+                    marginBottom: 4
+                  }}
+                >
                 {summaryCard}
+                </Box>
                 <VerticalTimeline>
                     {jobs}
                 </VerticalTimeline>
-            </div>
+            </Box>
         );
     }
 }

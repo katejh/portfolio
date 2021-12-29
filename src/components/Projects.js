5147fab3649f0c1714f5d3696412fff007012c12
Kate Huang <katejhuang@hotmail.com>
2021-12-29 09:00:58 -0800

    clean up Projects code a little

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,48 +3,38 @@ import { Typography, Box, Grid } from '@mui/material';
 
 function Projects(props) {
     return (
-        <div>
-            <Box
-              sx={{
-                m: 4
-              }}
-            >
-            <div
-              style={{
-                display: 'flex',
-              }}
-            >
-                <Typography variant="h4">
-                    Projects
-                </Typography>
-            </div>
-            <div
-              style={{
-                display: 'flex',
-                alignContent: 'center',
-                //justifyContent: 'center',
-              }}
-            >  
-                <Box sx={{mt: 4}}>          
-                    <ProjectsList 
-                      projects={props.projects} 
-                      cardProps={{
-                        sx: {
-                          width: 250,
-                          height: 200,
-                          cursor: 'pointer'
-                        },
-                      }}
-                      gridProps={{
-                        container: true,
-                        spacing: 2
-                      }}
-                      showModal
-                    />
-                </Box>            
-            </div>
-            </Box>
+      <Box sx={{m: 4}}>
+        <div style={{display: 'flex'}}>
+            <Typography variant="h4">
+                Projects
+            </Typography>
         </div>
+        <div
+          style={{
+            display: 'flex',
+            alignContent: 'center',
+            //justifyContent: 'center',
+          }}
+        >  
+            <Box sx={{mt: 4}}>          
+                <ProjectsList 
+                  projects={props.projects} 
+                  cardProps={{
+                    sx: {
+                      width: 250,
+                      height: 200,
+                      cursor: 'pointer'
+                    },
+                  }}
+                  gridProps={{
+                    container: true,
+                    spacing: 2
+                  }}
+                  showModal
+                />
+            </Box>            
+        </div>
+      </Box>
     );
 }
 
